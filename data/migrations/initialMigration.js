a050e9cd15c231cd87f8fbdd08e2a527b74e1b13
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-27 20:25:03 -0500

    Fixed seed and migration file.

--- data/migrations/initialMigration.js
+++ data/migrations/initialMigration.js
@@ -1,28 +1,15 @@
 exports.up = function (knex) {
   return knex.schema
   .createTable('books', table => {
-    table.increments('book_id');
-    table.text('book_name', 128)
-    .unique()
-    .notNullable()
-  })
-  .createTable('authors', table => {
-    table.increments('author_id');
+    table.increments('book_id')
+    table.text('book_title', 128)
     table.text('author_name', 128)
     .unique()
     .notNullable()
-    table.integer('book_id')
-    .unsigned()
-    .notNullable()
-    .references('book_id')
-    .inTable('books')
-    .onUpdate('RESTRICT')
-    .onDelete('RESTRICT') 
   })
 }
 
 exports.down = function (knex) {
   return knex.schema
-  .dropTableIfExists('authors')
   .dropTableIfExists('books')
 }
--- data/seeds/01-books.js
+++ data/seeds/01-books.js
@@ -1,9 +1,9 @@
 const books = [
-  {book_title: 'The Alchemist' },
-  {book_title: 'The Great Gatsby' },
-  {book_title: 'To Kill a Mockingbird'},
-  {book_title: 'One Hundred Years of Solitude' },
-  {book_title: 'The Catcher in the Rye' }
+  {book_title: 'The Alchemist', author_name: 'Paulo Coelho' },
+  {book_title: 'The Great Gatsby', author_name: 'F. Scott Fitzgerald' },
+  {book_title: 'To Kill a Mockingbird', author_name: 'Harper Lee' },
+  {book_title: 'One Hundred Years of Solitude', author_name: 'Gabriel Garcia Marquez' },
+  {book_title: 'The Catcher in the Rye', author_name: 'J.D. Salinger' }
 ]
 
 exports.books = books
