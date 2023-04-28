1f02aaca3b72eb7551b655063246369d297e5c84
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-28 11:22:29 -0500

    created router.test.js along with functions. Tasks have been completed and MVP was met

--- api/books/router.test.js
+++ api/books/router.test.js
@@ -1,7 +1,7 @@
 const db = require('../../db-config')
 const request = require('supertest')
-const server = require('../books/router')
-const Book = require('../books/model')
+const server = require('../../server')
+
 
 beforeAll(async () => {
   await db.migrate.rollback()
@@ -12,17 +12,17 @@ beforeEach(async () => {
   await db.seed.run()
 })
 
-// describe('[GET] /books', () => {
-//   test('responds with a 200 ok', async () => {
-//     const res = await request(server).get('/books')
-//     expect(res.status).toBe(200)
-//   }, 10000)
+describe('[GET] /api/books', () => {
+  test('responds with a 200 ok', async () => {
+    const res = await request(server).get('/api/books')
+    expect(res.status).toBe(200)
+  })
 
-//   test('responds with all the books', async () => {
-//     const res = await request(server).get('/books')
-//     expect(res.status).toBe(200)
-//     expect(res.body).toBeGreaterThan(0)
-//   }, 10000)
-// })
+  test('responds with all the books', async () => {
+    const res = await request(server).get('/api/books')
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveLength(5)
+  })
+})
 
 
